9ce1dd9c68c358b12443134b1cc04131a41fafb6
laeba <laeba2704@gmail.com>
2024-10-17 00:15:05 +0530

    huhh

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function LandingPage() {
 
         <Link href="/generate" passHref className="mb-12 md:mb-16  w-auto ">
           <button className=" flex gap-1 shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
-          Start Creating Now
+          Start Creating Now 
           <ArrowRight className="ml-2 h-4 w-4 md:h-5 md:w-5" />
           </button>
         </Link>
